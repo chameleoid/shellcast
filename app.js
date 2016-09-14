0537f1b9d470915c2a787ea9cd2b491d1b13e1cf
rummik <k@9k1.us>
2016-09-13 21:22:59 -0400

    Mass indent change

--- app.js
+++ app.js
@@ -14,46 +14,46 @@ var _ = require('underscore');
 
 var shellcasts = {};
 var stats = {
-	_viewers: 0,
-	_broadcasts: 0,
-	publicBroadcasts: {},
-	socket: (function() {
-		var socket = io.of('/stats');
+  _viewers: 0,
+  _broadcasts: 0,
+  publicBroadcasts: {},
+  socket: (function() {
+    var socket = io.of('/stats');
 
-		socket.on('connection', function(socket) {
-			stats.update();
-		});
+    socket.on('connection', function(socket) {
+      stats.update();
+    });
 
-		return socket;
-	})(),
+    return socket;
+  })(),
 
-	update: _.debounce(function() {
-		this.socket.emit('stats', {
-			viewers: this._viewers,
-			broadcasts: this._broadcasts,
-			publicBroadcasts: this.publicBroadcasts,
-		});
-	}),
+  update: _.debounce(function() {
+    this.socket.emit('stats', {
+      viewers: this._viewers,
+      broadcasts: this._broadcasts,
+      publicBroadcasts: this.publicBroadcasts,
+    });
+  }),
 };
 
 Object.defineProperties(stats, {
-	viewers: {
-		get: function() { return stats._viewers; },
+  viewers: {
+    get: function() { return stats._viewers; },
 
-		set: function(val) {
-			stats._viewers = val;
-			stats.update();
-		},
-	},
+    set: function(val) {
+      stats._viewers = val;
+      stats.update();
+    },
+  },
 
-	broadcasts: {
-		get: function() { return stats._broadcasts; },
+  broadcasts: {
+    get: function() { return stats._broadcasts; },
 
-		set: function(val) {
-			stats._broadcasts = val;
-			stats.update();
-		},
-	},
+    set: function(val) {
+      stats._broadcasts = val;
+      stats.update();
+    },
+  },
 });
 
 
@@ -66,13 +66,13 @@ app.set('view cache', false);
 swig.setDefaults({ cache: false });
 
 if (process.env.NODE_ENV == 'production') {
-	app.set('view cache', true);
+  app.set('view cache', true);
 }
 
 
 app.use(function(req, res, next) {
-	console.log('%s %s', req.method, req.path);
-	next();
+  console.log('%s %s', req.method, req.path);
+  next();
 });
 
 app.use('/t', term.middleware());
@@ -80,93 +80,93 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', function(req, res) {
-	res.render('home');
+  res.render('home');
 });
 
 app.get('/t/:term', function(req, res) {
-	if (shellcasts[req.params.term]) {
-		res.render('shell');
-	} else {
-		res.status(404).send('No shell broadcasting at this location.');
-	}
+  if (shellcasts[req.params.term]) {
+    res.render('shell');
+  } else {
+    res.status(404).send('No shell broadcasting at this location.');
+  }
 });
 
 app.get('/t/:term/c', function(req, res) {
-	if (!shellcasts[req.params.term]) {
-		var token = Math.random();
-		var sc = shellcasts[req.params.term] = {
-			term: term(),
-			viewers: 0,
-			socket: io.of('/t/' + req.params.term),
-			control: io.of('/t/' + req.params.term + '/' + token),
-			options: {
-				public: false,
-				title: req.params.term,
-			},
-		};
+  if (!shellcasts[req.params.term]) {
+    var token = Math.random();
+    var sc = shellcasts[req.params.term] = {
+      term: term(),
+      viewers: 0,
+      socket: io.of('/t/' + req.params.term),
+      control: io.of('/t/' + req.params.term + '/' + token),
+      options: {
+        public: false,
+        title: req.params.term,
+      },
+    };
 
-		sc.term.open(document.body);
+    sc.term.open(document.body);
 
-		sc.control.on('connection', function(socket) {
-			socket.on('data', function(data) {
-				sc.term.write(data);
-				sc.socket.emit('data', data);
-			});
+    sc.control.on('connection', function(socket) {
+      socket.on('data', function(data) {
+        sc.term.write(data);
+        sc.socket.emit('data', data);
+      });
 
-			socket.on('resize', function(data) {
-				sc.term.options.cols = data.cols;
-				sc.term.options.rows = data.rows;
-				sc.term.resize(data.cols, data.rows);
-				sc.socket.emit('resize', data);
-			});
+      socket.on('resize', function(data) {
+        sc.term.options.cols = data.cols;
+        sc.term.options.rows = data.rows;
+        sc.term.resize(data.cols, data.rows);
+        sc.socket.emit('resize', data);
+      });
 
-			socket.on('options', function(options) {
-				var del = !sc.options.public && options.public;
+      socket.on('options', function(options) {
+        var del = !sc.options.public && options.public;
 
-				sc.options = options;
+        sc.options = options;
 
-				if (options.public) {
-					stats.publicBroadcasts[req.params.term] = _.pick(sc, 'viewers', 'options');
-				} else if (del) {
-					delete stats.publicBroadcasts[req.params.term];
-				}
+        if (options.public) {
+          stats.publicBroadcasts[req.params.term] = _.pick(sc, 'viewers', 'options');
+        } else if (del) {
+          delete stats.publicBroadcasts[req.params.term];
+        }
 
-				stats.update();
-			});
+        stats.update();
+      });
 
-			stats.broadcasts++;
+      stats.broadcasts++;
 
-			socket.on('disconnect', function() {
-				stats.broadcasts--;
+      socket.on('disconnect', function() {
+        stats.broadcasts--;
 
-				sc.term.reset();
-				delete shellcasts[req.params.term];
-				delete stats.publicBroadcasts[req.params.term];
-			});
-		});
+        sc.term.reset();
+        delete shellcasts[req.params.term];
+        delete stats.publicBroadcasts[req.params.term];
+      });
+    });
 
-		sc.socket.on('connection', function(socket) {
-			socket.emit('sync', {
-				lines: sc.term.lines,
-				x: sc.term.x,
-				y: sc.term.y,
-				rows: sc.term.rows,
-				cols: sc.term.cols,
-			});
+    sc.socket.on('connection', function(socket) {
+      socket.emit('sync', {
+        lines: sc.term.lines,
+        x: sc.term.x,
+        y: sc.term.y,
+        rows: sc.term.rows,
+        cols: sc.term.cols,
+      });
 
-			stats.viewers++;
+      stats.viewers++;
 
-			sc.socket.emit('viewers', ++sc.viewers);
-			socket.on('disconnect', function() {
-				stats.viewers--;
+      sc.socket.emit('viewers', ++sc.viewers);
+      socket.on('disconnect', function() {
+        stats.viewers--;
 
-				sc.viewers--;
-				sc.socket.emit('viewers', sc.viewers);
-			});
-		});
+        sc.viewers--;
+        sc.socket.emit('viewers', sc.viewers);
+      });
+    });
 
-		res.json({ token: token });
-	}
+    res.json({ token: token });
+  }
 });
 
 
--- public/t/shellcast.js
+++ public/t/shellcast.js
@@ -3,12 +3,12 @@ Terminal.bindKeys =
 Terminal.fixIpad = function() {};
 
 var term = new Terminal({
-	cols: 80,
-	rows: 24,
-	useStyle: true,
-	scrollback: 0,
-	cursorBlink: false,
-	colors: Terminal.tangoColors,
+  cols: 80,
+  rows: 24,
+  useStyle: true,
+  scrollback: 0,
+  cursorBlink: false,
+  colors: Terminal.tangoColors,
 });
 
 term.scaleMode = 'zoom';
@@ -20,54 +20,54 @@ var socket = io.connect(location.protocol + '//' + location.host + location.path
 socket.on('connect', term.reset.bind(term));
 socket.on('data', term.write.bind(term));
 socket.on('viewers', function(viewers) {
-	document.getElementById('viewers').innerHTML = viewers;
+  document.getElementById('viewers').innerHTML = viewers;
 });
 
 socket.on('resize', function(data) {
-	term.options.cols = data.cols;
-	term.options.rows = data.rows;
-	term.resize(data.cols, data.rows)
-	setTimeout(window.onresize, 0);
+  term.options.cols = data.cols;
+  term.options.rows = data.rows;
+  term.resize(data.cols, data.rows)
+  setTimeout(window.onresize, 0);
 });
 
 socket.on('sync', function(data) {
-	term.options.cols = data.cols;
-	term.options.rows = data.rows;
-	term.resize(data.cols, data.rows);
-	setTimeout(function() {
-		term.lines = data.lines;
-		term.x = data.x;
-		term.y = data.y;
-		term.refresh(0, term.rows - 1);
-		window.onresize();
-	}, 0);
+  term.options.cols = data.cols;
+  term.options.rows = data.rows;
+  term.resize(data.cols, data.rows);
+  setTimeout(function() {
+    term.lines = data.lines;
+    term.x = data.x;
+    term.y = data.y;
+    term.refresh(0, term.rows - 1);
+    window.onresize();
+  }, 0);
 });
 
 // TODO: display a notification
 socket.on('disconnect', function() {
-	term.reset();
-	term.write('Disconnected.');
+  term.reset();
+  term.write('Disconnected.');
 });
 
 (onresize = function() {
-	var transform;
-	var scaleX = window.innerWidth / term.element.clientWidth;
-	var scaleY = window.innerHeight / term.element.clientHeight;
+  var transform;
+  var scaleX = window.innerWidth / term.element.clientWidth;
+  var scaleY = window.innerHeight / term.element.clientHeight;
 
-	switch (term.scaleMode) {
-		case 'stretch':
-			transform = 'scaleX(' + scaleX + ') scaleY(' + scaleY + ')';
-			break;
+  switch (term.scaleMode) {
+    case 'stretch':
+      transform = 'scaleX(' + scaleX + ') scaleY(' + scaleY + ')';
+      break;
 
-		case 'zoom':
-		default:
-			transform = 'scale(' + Math.min(scaleX, scaleY) + ')';
-			break;
-	}
+    case 'zoom':
+    default:
+      transform = 'scale(' + Math.min(scaleX, scaleY) + ')';
+      break;
+  }
 
-	term.element.style.marginTop = -(term.element.clientHeight / 2) + 'px';
-	term.element.style.marginLeft = -(term.element.clientWidth / 2) + 'px';
+  term.element.style.marginTop = -(term.element.clientHeight / 2) + 'px';
+  term.element.style.marginLeft = -(term.element.clientWidth / 2) + 'px';
 
-	term.element.style.transform =
-	term.element.style.webkitTransform = transform;
+  term.element.style.transform =
+    term.element.style.webkitTransform = transform;
 })();
